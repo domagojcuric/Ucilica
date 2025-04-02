c31293bb974ae4385a88830d3c1d31565ffab496
Domagoj <curic.domagoj993@gmail.com>
2025-04-02 12:22:57 +0200

    dodana ucilica

---
+++ src/Ucilica.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Ucilica {
+
+    ArrayList<GeometrijskiLik> likovi;
+
+
+    Ucilica(){
+        likovi = new ArrayList<>();
+    }
+
+    public ArrayList<GeometrijskiLik> getLikovi() {
+        return likovi;
+    }
+}
